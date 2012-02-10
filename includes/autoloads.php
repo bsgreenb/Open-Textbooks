a19c468cb0513ade99b171e58969e49ceba786fa
Ben Greenberg <bsgreenb@gmail.com>
2012-02-09 17:42:47 -0800

    autoloads

---
+++ includes/autoloads.php
@@ -0,0 +1,21 @@
+<?php
+
+//error_reporting(0); //report all errors during development..
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+//config..
+require_once($root . '/../includes/db_config.php');
+
+//includes..
+require_once($root . '/../includes/book_functions.php');
+require_once($root . '/../includes/bookstore_functions.php');
+require_once($root . '/../includes/date_functions.php');
+require_once($root . '/../includes/db_functions.php');
+require_once($root . '/../includes/display_functions.php');
+require_once($root . '/../includes/math_functions.php');
+require_once($root . '/../includes/parsing_functions.php');
+require_once($root . '/../includes/url_functions.php');
+require_once($root . '/../includes/validation_functions.php');
+
+?>
