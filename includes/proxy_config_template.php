babe22924858b1f8440a4571b460132abbfc21f7
Ben Greenberg <bsgreenb@gmail.com>
2012-03-08 23:34:01 -0800

    Adding note on specifying port

--- includes/proxy_config_template.php
+++ includes/proxy_config_template.php
@@ -3,6 +3,8 @@
 //Proxies are necessary to bypass Follets and Barnes and Nobles rate limiting.
 //Services I recommend for proxies.  Low-traffic: proxymesh, AWS cloud, or rackspace cloud.  High-traffic: trustedproxies, hide my ass
 
+//NOTE: Make sure to specify the Port in addition to host
+
 define('PROXY_1', '');
 define('PROXY_1_AUTH', '');
 
